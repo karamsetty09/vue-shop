449c3758eb036b794695f323054f31c1da229513
venkata <sandeepsandy2530@gmail.com>
2021-09-17 19:58:07 +1000

    Adding data in component and scoped(local) styles for component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-      <h4>{{title}}</h4>
+  <div class="event-card -shadow">
+      <span class="eyebrow">@{{event.time}} on {{ event.date }}</span>
+      <h4 class="title">{{event.title}}</h4>
+      <span>{{event.attendees.length}} attending </span>
   </div>
 </template>
 
@@ -8,14 +10,40 @@
 export default {
     data(){
         return {
-            title: 'Beach Cleanup'
+            event: {
+                id: 1,
+                title: 'Beach cleanup',
+                data: 'Tues Aug 19, 2018',
+                time: '6:00',
+                attendees:[
+                    {id: 'abc123', name: 'Venkata'},
+                    {id: 'def456', name: 'Karamsetty'}
+                ]
+
+            }
         }
     }
 }
 </script>
 
-<style>
-h4{
-    color: green;
-}
-</style>
+<style scoped>
+    .event-card {
+      padding: 20px;
+      margin-bottom: 24px;
+      transition: all 0.2s linear;
+      cursor: pointer;
+    }
+    .event-card:hover {
+      transform: scale(1.01);
+      box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+    }
+    .event-card > .title {
+      margin: 0;
+    }
+    
+    .event-link {
+      color: black;
+      text-decoration: none;
+      font-weight: 100;
+    }
+    </style>
