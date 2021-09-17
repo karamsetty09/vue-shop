579729aeb3181c24efb19e3ba9d245f33afdbb7f
venkata <sandeepsandy2530@gmail.com>
2021-09-17 19:43:48 +1000

    Adding component in components and display in views.

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,19 @@
 <template>
 <div>
   <h1>Events Listing</h1>
+
+  <EventCard/>
   <router-link :to="{name:'event-show'}">Show Event #1</router-link> 
 </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
 
