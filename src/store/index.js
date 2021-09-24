dc45403030510e4dc9987fd44ab6f7a2f6d0ae26
venkata <sandeepsandy2530@gmail.com>
2021-09-25 05:18:26 +1000

    Adding mutaions and actions to insert records into db.

--- src/store/index.js
+++ src/store/index.js
@@ -1,46 +1,56 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import EventService from "@/services/EventService.js";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: {id:'123abc', name: 'venkata Karamsetty'},
+    user: { id: "123abc", name: "venkata Karamsetty" },
     categories: [
-      'sustainability',
-      'nature',
-      'animal welfare',
-      'housing',
-      'education',
-      'food',
-      'community'
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "community",
     ],
     todos: [
-      { id: 1, text: '...', done: true },
-      { id: 2, text: '...', done: false },
-      { id: 3, text: '...', done: true },
-      { id: 4, text: '...', done: false }
-    ]
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+      { id: 3, text: "...", done: true },
+      { id: 4, text: "...", done: false },
+    ],
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event);
+    },
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit("ADD_EVENT", event);
+      });
+    },
+  },
   modules: {},
   getters: {
-    catLength: state => {
-      return state.categories.length
+    catLength: (state) => {
+      return state.categories.length;
     },
-    doneTodos: state =>{
-      return state.todos.filter(todo => todo.done)
+    doneTodos: (state) => {
+      return state.todos.filter((todo) => todo.done);
     },
     getEventById: (state) => (id) => {
-      return state.todos.find(event => event.id === id)
+      return state.todos.find((event) => event.id === id);
     },
-    activeTodosCount: (state)=>{
-      return state.todos.filter(todo => !todo.done).length
+    activeTodosCount: (state) => {
+      return state.todos.filter((todo) => !todo.done).length;
     },
     // Another way of doing using normal logic
     // activeTodosCount: (state, getters) => {
     //   return state.todos.length - getters.doneTodos.length
     // }
-  }
+  },
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,52 +1,116 @@
 <template>
-<div>
-  <h1>Create Event, {{user.id}}</h1>
-  <p> This event was created by {{user.name}} </p>
-  <p>{{ getEventById(2) }}</p>
-  <p> Length of Categories: {{catLength}}</p>
-  <ul>
-    <li v-for="category in categories" :key="category">{{category}}</li>
-  </ul>
-  
-</div>
-  
+  <div>
+    <h1>Create an Event</h1>
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
+      </div>
+
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
+      </div>
+
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
+      </div>
+
+      <h3>When is your event?</h3>
+
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date" />
+      </div>
+
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
+  </div>
 </template>
 
-
 <script>
-
-import {mapState, mapGetters} from 'vuex'
+import Datepicker from "vuejs-datepicker";
 
 export default {
+  components: {
+    Datepicker,
+  },
+  data() {
+    const times = [];
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ":00");
+    }
+    return {
+      times,
+      categories: this.$store.state.categories,
+      event: this.createFreshEventObject(),
+    };
+  },
+  methods: {
+    createEvent() {
+      this.$store
+        .dispatch("createEvent", this.event)
+        .then(() => {
+          this.$router.push({
+            name: "event-show",
+            params: { id: this.event.id },
+          });
+          this.event = this.createFreshEventObject();
+        })
+        .catch(() => {
+          alert("There was a problem creating your event");
+        });
+    },
+    createFreshEventObject() {
+      const user = this.$store.state.user;
+      const id = Math.floor(Math.random() * 10000000);
 
-// easy way to use array when no renaming is required.
-computed: {
-  ...mapGetters(['getEventById','catLength']),
-  ...mapState(['user', 'categories'])
-  }
-
-//Easy way for renaming and calling the store variables.
-// computed: mapState({
-//   user: 'user',
-//   categories: 'categories'
-// })
-
-// Reagular way of declaring.
-// computed: mapState({
-//   userName: state => state.user.name,
-//   userID: state => state.user.id,
-//   categories: state => state.categories
-// })
-
-//   one way of linking store data to variable for display
-// computed: {
-//   userName(){
-//     return this.$store.state.user.name
-//   }
-// }
-}
+      return {
+        id: id,
+        user: user,
+        category: "",
+        organizer: user,
+        title: "",
+        description: "",
+        location: "",
+        date: "",
+        time: "",
+        attendees: [],
+      };
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
 </style>
